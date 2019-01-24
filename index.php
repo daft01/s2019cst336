7ab16b609b136680365c4e69725424854fc5d93e
Daniel Morales <danmorales@csumb.edu>
2019-01-24 06:39:03 +0000

    deleted index.html and added html to the hello-world.php

--- index.php
+++ index.php
@@ -1,4 +1,7 @@
-<html><body>
+<html>
+    <title>Daniel's Website</title>
+    <body>
+        <h1>BOIII</h1>
 <?php
 // A simple web site in Cloud9 that runs through Apache
 // Press the 'Run' button on the top to start the web server,
@@ -7,5 +10,5 @@
 echo 'Hello world from Cloud9!';
 
 ?>
-</body>
+    </body>
 </html>
