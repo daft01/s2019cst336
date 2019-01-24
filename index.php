67e2e725b52bab61b8e0118faf97bfaafbf57cf3
Daniel Morales <danmorales@csumb.edu>
2019-01-24 09:04:36 +0000

    deleted index.html and added html to the hello-world.php

--- index.php
+++ index.php
@@ -1,6 +1,16 @@
 <html>
     <title>Daniel's Website</title>
+    <link href="index.css" rel="stylesheet" type="text/css">
     <body>
-        <h1>BOiii !!</h1>
+        <ul>
+            <li>Home</li>
+            <li>About</li>
+            <li>Resume</li>
+            <li>Contact</li>
+        </ul>
+        
+        <h1>Daniel Morales</h1>
+        <h2>Developer</h2>
+        <p>First generation college student passionate about problem-solving and helping to improve people's lives through technology.</p>
     </body>
 </html>
