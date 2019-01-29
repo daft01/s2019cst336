5146b543df21def7503afb35e9a4401d0dec9ae2
Daniel Morales <danmorales@csumb.edu>
2019-01-29 00:47:54 +0000

    something

--- index.php
+++ index.php
@@ -1,27 +1,7 @@
 <html>
     <title>Daniel's Website</title>
-    <link href="index.css" rel="stylesheet" type="text/css">
+        
     <body>
-        <ul>
-            <li><a href="">Home</a></li>
-            <li><a href="">About</a></li>
-            <li><a href="https://drive.google.com/file/d/1E9yirylJO2GzkPrL_8-VkKineCrIzBSJ/view?usp=sharing">Resume</a></li>
-            <li><a href="https://drive.google.com/drive/folders/1rv910JSA6E8RDCo0tDbGliI-HNRYOc_j?usp=sharing">Reference Letters</a></li>
-            <li style="float:right"><a class="active" href="">Contact</a></li>
-        </ul>
-        
-        <div class="row">
-            
-            <div id="info">
-                <h1>Daniel Morales</h1>
-                <h2>Developer</h2>
-                <p>First generation college student passionate about problem-solving and helping to improve people's lives through technology.</p>
-            </div>
-            
-            <div id="picture">
-                <img src="ProfilePicture.png" alt="Profile Picture">
-            </div> 
-        </div>
-        
+
     </body>
 </html>
