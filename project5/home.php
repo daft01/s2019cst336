4ce81357c38650be7d3b3b56d12158ca334ecc9c
Daniel Morales <danmorales@csumb.edu>
2019-05-05 20:31:12 +0000

    lol

--- project5/home.php
+++ project5/home.php
@@ -1,51 +1,210 @@
 <!DOCTYPE html>
 <html>
-    <head>
-        <title> </title>
-        <link rel="stylesheet" href="css/styles.css">
-    </head>
-    <body>
+
+<head>
+    <title></title>
+  <style>
+  
+    body{
+      background-image: url("background.jpg");
+      font-size: 2em;
+      color: black;
+    }
     
-    <div class="container">
-        <div id="nav">
-            <a href="home.html" id="home">Home</a>
-            |
-            <a href="index.html" id="upload">Upload</a>
+    img{
+      background-color: white;
+    }
+    
+    input{
+      margin: 10px;
+    }
+    
+    #info {
+      display: flex;
+      margin: 5px;
+    }
+    button{
+      margin: 5px;
+    }
+
+    #box{
+      display: flex;
+    }
+    .caption{
+      margin: 10px;
+      font-size: 1.5em;
+    }
+  </style>
+</head>
+
+<body>
+<div id="container">
+  <form method="POST" enctype="multipart/form-data">
+    
+    <div id=info>
+      
+      <label>Email:</label><br>
+      <div id="emailDiv">
+        <input type="text" name="email" id="email"/><br>
+        <div id="results"></div>
+      </div>
+      
+      <label>Caption:</label><br>
+        <textarea name="textbox" cols="40" rows="4" placeholder="insert caption"> </textarea> <br>
+      
+        <div style="display:none;">
+            <input type="file" multiple name="fileName" />
+        </div>
+    
+        <div>
+            <button id="selectButton" type="button" class="btn btn-primary btn-xs">Pick File(s)</button>
         </div>
         
-        <div id="content">
-            <?php
-                $db = mysqli_connect("localhost","root","","project5"); 
-                $sql = "SELECT * FROM up_files WHERE 1";
-                $sth = $db->query($sql);
-                while($result=mysqli_fetch_array($sth))
-                {
-                    echo $result['media'];
-                    echo '<img style="width:300px; height:150px;" src="data:image/jpeg;base64,'. base64_encode( $result['media'] ) . '"/>';
-                }
-            ?>
-        </div>
+        <div id="filesList"></div>
         
+        <div>
+            <button id="uploadButton" type="button" class="btn btn-primary btn-xs">Upload File(s)</button>
+        </div>
+          
+        <div class="progress-bar" role="progressbar" aria-valuenow="0" aria-valuemin="0" aria-valuemax="100" style="width: 0%;">
+            0%
+        </div>
+  
+    
     </div>
+  </form>
+
+  <div id="outer_wrapper">
+    <div id="inner_wrapper">
+       <?php
+          
+          if  (strpos($_SERVER['HTTP_HOST'], 'herokuapp') !== false)
+            $db = mysqli_connect($url["host"], $url["user"], $url["pass"], substr($url["path"], 1)); 
+          else
+            $db = mysqli_connect("localhost","root","","project5"); 
+          
+          $sql = "SELECT * FROM up_files WHERE 1";
+          $sth = $db->query($sql);
+          echo "<div id='box'>";
+          $i = 1;
+          
+          while($result=mysqli_fetch_array($sth))
+          {
+              echo "<div>";
+              echo '<img style="height:600px;width:auto "src="data:image/jpeg;base64,'.base64_encode( $result['media'] ).'"/>';
+              echo '<div class="caption"> ' . $result['caption'] . '</div>';
+              echo "</div>";
+            $i += 1;
+          }
+          echo "</div>";
+       ?>
+    </div>
+  </div>
+</div>
+
+<!-- jQuery -->
+<script src="https://ajax.googleapis.com/ajax/libs/jquery/1.12.4/jquery.min.js"></script>
+<script src="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/js/bootstrap.min.js" integrity="sha384-Tc5IQib027qvyjSMfHjOMaLkfuWVxZxUPnCJA7l2mCWNIpG9mGCD8wGNIcPD7Txa" crossorigin="anonymous"></script>
+
+<script type="text/javascript">
+/* global $ */
+  // 1. Get rid of file input button
+  //$("form button:nth-of-type(1)").click(function() {
+  $("#selectButton").click(function() {
+    $("form input[type='file']").trigger("click")
+  })
+
+  // 2. Use ajax to submit files
+  $("form input[type='file']").change(function(e) {
     
-    <script src="https://ajax.googleapis.com/ajax/libs/jquery/1.12.4/jquery.min.js"></script>
-    <script src="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/js/bootstrap.min.js" integrity="sha384-Tc5IQib027qvyjSMfHjOMaLkfuWVxZxUPnCJA7l2mCWNIpG9mGCD8wGNIcPD7Txa" crossorigin="anonymous"></script>
-    <script type="text/javascript">
+    $('#filesList').empty();
     
-        var images = [];
+    $.map(this.files, function(val) {
+      $('#filesList')
+        .append($('<div>')
+          .html(val.name)
+        );
+    });
+    
+  });
+
+    // 3. Send files with ajax
+  $('#uploadButton').click(function(e) {
+      setProgress(0);
+      
+      var email = $('#email').val()
+      
+      var at = false;
+      var point = false;
+      
+      for(var i=0; i < email.length; i++){
         
-        $.ajax({
-            type:"GET",
-            url:"getData.php",
-            dataType: "json",
-            success: function(data, status){
-            
-                console.log(data);
-            },
-            error: function(err) {
-                console.log(arguments);  
-            },
-        });
-    </script>
-    </body>
+        if(email[i] == "@"){
+          at = true;
+        }
+        
+        if(email[i] == '.' && at){
+          point = true;
+        }
+        
+      }
+      
+      if(!point){
+        document.getElementById("results").innerHTML = "Invalid Email";
+        return;
+      }
+      var formData = new FormData($('form')[0]);
+      $.ajax({
+        url: "uploadFile.php",
+        type: "POST",
+        data: formData,
+        processData: false,
+        contentType: false,
+        mimeType: "multipart/form-data",
+        cache: false,
+        // This part gives up chunk progress of the file upload
+        xhr: function() {
+          //upload Progress
+          var xhr = $.ajaxSettings.xhr();
+          if (xhr.upload) {
+            xhr.upload.addEventListener('progress', function(event) {
+              var percent = 0;
+              var position = event.loaded || event.position;
+              var total = event.total;
+              if (event.lengthComputable) {
+                  percent = Math.ceil(position / total * 100);
+              }
+              //update progressbar
+              setProgress(percent);
+            }, true);
+          }
+          return xhr;
+        }
+      })
+      .done(function(data, status, xhr) {
+          console.log('upload done');
+          //window.location.href = "<?php echo BASE_PATH?>/assets/<?php echo $controller->group ?>";
+          console.log(xhr);
+          $("#results").html(xhr.responseText)
+      })
+      .fail(function(xhr) {
+          console.log('upload failed');
+          console.log(xhr);
+      })
+      .always(function() {
+          //console.log('done processing upload');
+      });
+  });
+
+  function setProgress(percent) {
+    $(".progress-bar").css("width", + percent + "%");
+    $(".progress-bar").text(percent + "%");
+  }
+  
+  function heartClicked(image) {
+    
+  }
+</script>
+</body>
+
 </html>
--- project5/uploadFile.php
+++ project5/uploadFile.php
@@ -1,44 +1,26 @@
 <?php 
-// http://php.net/manual/en/features.file-upload.multiple.php
-
-
-    $file_ary = reArrayFiles($_FILES['fileName']);
-
-    foreach ($file_ary as $file) {
-        if ($file["error"] > 0) {
-          echo "Error: " . $file["error"] . "<br>";
-        }
-        else {
-          echo "Upload: " . $file["name"] . "<br>";
-          echo "Type: " . $file["type"] . "<br>";
-          echo "Size: " . ($file["size"] / 1024) . " KB<br>";
-          echo "Stored in: " . $file["tmp_name"] . "<br>";$_POST["email"] . "<br>";
-          echo $_POST["caption"] . "<br>";
-        }  
-        
-        include 'dbConn.php';
+    if ($_FILES["fileName"]["error"] > 0) {
+    echo "Error: " . $_FILES["fileName"]["error"] . "<br>";
+    } else {
+    echo "Upload: " . $_FILES["fileName"]["name"] . " | ";
+    echo "Type: " . $_FILES["fileName"]["type"] . " | ";
+    echo "Size: " . ($_FILES["fileName"]["size"] / 1024) . " KB | ";
+    echo "Stored in: " . $_FILES["fileName"]["tmp_name"];
+    }
     
-    $binaryData = file_get_contents($file["tmp_name"]);
+    include 'dbConn.php';
+    
+    if( $_FILES["fileName"]["size"] / 1024 > 10000){
+        echo "File needs to be less then 10 MB";
+    }else{
+    
+    $file = addslashes(file_get_contents($_FILES["fileName"]["tmp_name"]));
     $date = "SELECT NOW()";
-    $sql = "INSERT INTO up_files (email_address, caption, media, timestamp) " . "  VALUES ('" . $_POST['email'] . "', '" . $_POST['caption'] . "', '" . $file . "', '" . $data . "') ";
-    $stm=$dbConn->prepare($sql);
+    $sql = "INSERT INTO `up_files`(`email`, `caption`, `media`, `timestamp`) VALUES ('" . $_POST['email'] . "', '" . $_POST['textbox'] . "', '" . $file .  "', '" . $date . "')";
+    $stm= $dbConn->prepare($sql);
     $stm->execute();
-    echo "<br />File saved into database <br /><br />"; 
-        
+    echo "<br />File saved into database <br /><br />";
     }
 
-function reArrayFiles(&$file_post) {
-    $file_ary = array();
-    $file_count = count($file_post['name']);
-    $file_keys = array_keys($file_post);
-
-    for ($i=0; $i<$file_count; $i++) {
-        foreach ($file_keys as $key) {
-            $file_ary[$i][$key] = $file_post[$key][$i];
-        }
-    }
-
-    return $file_ary;
-}
 ?>
 
